1ede932b1fda316530aa3ad97607fe2d954271ca
Tammy Yang <tammy.yang@coinbase.com>
2022-02-01 09:37:18 +0000

    update scripts

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,23 @@
 require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-ethers')
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-module.exports = {
-  solidity: "0.8.0",
+ module.exports = {
+  networks: {
+    hardhat: {
+      accounts: {
+        mnemonic: 'test test test test test test test test test test test junk'
+      }
+    },
+    optimism: {
+      url: 'http://127.0.0.1:8545',
+      accounts: {
+        mnemonic: 'test test test test test test test test test test test junk'
+      },
+      gasPrice: 0
+    }
+  },
+  solidity: '0.8.0'
 };
