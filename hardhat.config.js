966e238f4478edaf7cd27673505a5e25ddfeaa86
Tammy Yang <tammy.yang@coinbase.com>
2022-02-02 15:17:23 +0000

    deploy and network config

--- hardhat.config.js
+++ hardhat.config.js
@@ -24,6 +24,16 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
         mnemonic: 'test test test test test test test test test test test junk'
       },
       gasPrice: 0
+    },
+    optimism_kovan: {
+      url: 'https://opt-kovan.g.alchemy.com/v2/{project_key}',
+      accounts: [private_key],
+      gasPrice: 0
+    },
+    ethereum_kovan: {
+      url: 'https://eth-kovan.alchemyapi.io/v2/{project_key}',
+      accounts: [private_key],
+      gasPrice: 0
     }
   },
   solidity: '0.8.9'
